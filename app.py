f0c9da91e345ec774a574fe628aa836f50ed4827
maniek3772 <tracz.marcin7@gmail.com>
2025-04-11 22:32:52 +0200

    Add new endpoints health and echo

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
+@app.route('/health')
+def health_check():
+    return jsonify({"status": "ok"})
+
+@app.route('/echo/<name>')
+def echo(name):
+    return jsonify({"message": f"Hello, {name}!"})
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
